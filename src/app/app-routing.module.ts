a207a589dc84168e13adc65bad3b544812839124
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-20 14:13:01 -0300

    criando um modulo de rotas cursos-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
