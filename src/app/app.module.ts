086a39b326e77c1cce63d1d3da15989ba0a732bf
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-20 14:46:35 -0300

    Rotas filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,20 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     CursosModule,
+    AlunosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
