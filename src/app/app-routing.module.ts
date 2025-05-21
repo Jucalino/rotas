1a600faaff38b1c527a5b63998634f22960850b3
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-21 11:40:41 -0300

    Guarda de rotas: CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
 
-  { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule)},
-  { path: 'alunos', loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule)},
-  { path: '', component: HomeComponent },
+  { path: 'cursos',
+     loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule),
+     canActivate: [AuthGuard]
+  },
+  { path: 'alunos',
+    loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule),
+    canActivate: [AuthGuard]
+  },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
